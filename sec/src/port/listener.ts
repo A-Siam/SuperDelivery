9611c180b5438f729d8f809f1409c038e22b19f4
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-21 18:19:49 +0000

    feat: create consumer

--- sec/src/port/listener.ts
+++ sec/src/port/listener.ts
@@ -1,12 +1,21 @@
+import { GROUP_ID } from "../constants";
 import TransactionLog from "../data/TranasctionLog";
 import { getKafkaClient } from "./kafka-connection";
 
-const consumer = getKafkaClient().consumer({groupId: GROUP_ID})
+const consumer = getKafkaClient().consumer({groupId: GROUP_ID!})
 
-function initConsumer() {
-    
+type OnMessageHandler = (message: TransactionLog) => void
+
+async function initConsumer(onMessage: OnMessageHandler) {
+    await consumer.run({
+        eachMessage: async ({topic, partition, message}) => {
+            onMessage(JSON.parse(message.value?.toString()!))
+        }
+    })
 }
 
 export async function initiateListener(topic: string, onMessageRecived: (message: TransactionLog) => void) {
-     
+    await consumer.connect()
+    await consumer.subscribe({topic, fromBeginning: true})
+    initConsumer(onMessageRecived)
 }
