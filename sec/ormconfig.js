057dacec0bd66faa52fdf26f74a2f54222e4593e
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-22 16:26:56 +0000

    add prisma

--- sec/ormconfig.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    type: 'postgres',
-    host: process.env['EVENTS_DB_HOST'],
-    port: parseInt(process.env['EVENTS_DB_PORT']),
-    username: process.env['EVENTS_DB_USER'],
-    password: process.env['EVENTS_DB_PASSWORD'],
-    database: process.env['EVENTS_DB_NAME'],
-}
