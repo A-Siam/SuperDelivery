ebcd9608c5e63ade6f4075fb44468c7ee17be558
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-22 13:40:12 +0000

    feat: add databases

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,12 @@
 -- transaction events db
 CREATE DATABASE transaction_events WITH OWNER = admin;
 
+-- orders db
+CREATE DATABASE orders WITH OWNER = admin;
+
+-- delivery db
+CREATE DATABASE delivery WITH OWNER = admin;
+
+-- events for sourcing
+CREATE DATABASE events WITH OWNER = admin;
+
