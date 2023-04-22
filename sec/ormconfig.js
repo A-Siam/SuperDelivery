65231ebde4ac8aae5ad00c6ab57dae4fada853c8
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-22 14:28:50 +0000

    remove storage to use ormconfig

--- sec/ormconfig.js
+++ sec/ormconfig.js
@@ -1,10 +1,8 @@
-import { DataSource } from 'typeorm'
-
-export const EventDataSource = new DataSource({
+module.exports = {
     type: 'postgres',
     host: process.env['EVENTS_DB_HOST'],
-    port: parseInt(process.env['EVENTS_DB_PORT']!),
+    port: parseInt(process.env['EVENTS_DB_PORT']),
     username: process.env['EVENTS_DB_USER'],
     password: process.env['EVENTS_DB_PASSWORD'],
     database: process.env['EVENTS_DB_NAME'],
-})
+}
