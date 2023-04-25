09fe5ef392f07ad6b5e0d77aed710b704857fef4
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-25 23:43:08 +0000

    feat: produce order event

--- order_service/src/saga/utils.go
+++ order_service/src/saga/utils.go
@@ -1,12 +1,11 @@
 package saga
 
 import (
-	"github.com/A-Siam/super_delivery/order_service/src/models"
 	"github.com/A-Siam/super_delivery/order_service/src/utils"
 	"github.com/Shopify/sarama"
 )
 
-func CreateSagaEventProducer(event models.Event) (*sarama.SyncProducer, error) {
+func CreateSagaEventProducer() (*sarama.SyncProducer, error) {
 	sagaQueueHost, err := utils.GetEnvWithCheck("KAFKA_BROKER_HOST_URL")
 	if err != nil {
 		return nil, err
--- order_service/src/service/order-service.go
+++ order_service/src/service/order-service.go
@@ -1,8 +1,13 @@
 package service
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/A-Siam/super_delivery/order_service/src/data/db"
 	"github.com/A-Siam/super_delivery/order_service/src/models"
+	"github.com/A-Siam/super_delivery/order_service/src/saga"
+	"github.com/Shopify/sarama"
 )
 
 func CreateOrder(orderName, owner string, price float32) (models.Order, error) {
@@ -23,16 +28,33 @@ func CreateOrder(orderName, owner string, price float32) (models.Order, error) {
 
 	otx := ordersDB.Create(&order)
 	etx := eventsDB.Create(&event)
-
 	if otx.Error != nil {
 		return models.Order{}, otx.Error
 	}
-
 	if etx.Error != nil {
 		return models.Order{}, etx.Error
 	}
-
-    producer, err := saga
-
+	produceOrderCreatedEvent(event)
 	return order, nil
+}
+
+func produceOrderCreatedEvent(event models.Event) error {
+	msgBytes, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+	producerPtr, err := saga.CreateSagaEventProducer()
+	if err != nil {
+		return err
+	}
+	msg := &sarama.ProducerMessage{
+		Topic: os.Getenv("EVENTS_TOPIC_NAME_URL"),
+		Value: sarama.StringEncoder(string(msgBytes)),
+	}
+	producer := *producerPtr
+	_, _, err = producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+	return nil
 }
