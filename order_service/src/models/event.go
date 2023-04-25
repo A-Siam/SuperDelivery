4a1c2e0c6d1eb0513bac9cab8b6828368cfe38a8
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-04-25 21:18:22 +0000

    feat: add gorm model

--- order_service/src/models/event.go
+++ order_service/src/models/event.go
@@ -2,9 +2,12 @@ package models
 
 import (
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type Event struct {
+	gorm.Model
 	EventName string `json:"event_name"`
 	Service   string `json:"service"`
 	CreatedAt time.Time
